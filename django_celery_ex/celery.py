f624b8d4a0828a7f2729e485b7d06f0091fc8dd8
holynekk <e238109@metu.edu.tr>
2022-08-02 15:35:14 +0300

    added make_thumbnails task

--- django_celery_ex/celery.py
+++ django_celery_ex/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_parroter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_ex.settings')
 
 celery_app = Celery('django_celery_ex')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
